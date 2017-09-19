4ebace1e458ef2fbaab65666d9e6cd82a3dd4af3
mvpoorna <mv.poorna@gmail.com>
2017-09-19 12:06:29 +0530

    first commit.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+    {id: 101, name: 'Ram'},
+    {id: 102, name: 'Charan'},
+    {id: 103, name: 'Balu'},
+    {id: 104, name: 'Rakesh'},
+    {id: 105, name: 'Prakash'}
+  ]
